e4c0b6d46ef0b8d69a74252821458dc87ee61faa
Akshith <akshithnukala9@gmail.com>
2021-05-29 21:50:23 +0530

    change procfile and url replaces

--- config.py
+++ config.py
@@ -28,14 +28,14 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL').replace('postgres','postgresql') or \
         'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
 
 class TestingConfig(Config):
     TESTING = True
     WTF_CSRF_ENABLED = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL').replace('postgres','postgresql') or \
         'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
 
 
